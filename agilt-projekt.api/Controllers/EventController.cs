a8c2ddeee22175dad87971a06b9af8a102baa133
mariahedkvist <mariasofiahedkvist@gmail.com>
2023-09-21 17:11:14 +0200

    HttpPost method

--- agilt-projekt.api/Controllers/EventController.cs
+++ agilt-projekt.api/Controllers/EventController.cs
@@ -1,9 +1,14 @@
+using agilt_projekt.api.ViewModel;
 using EventApi.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using EventApi.Models;
 
 namespace EventApi.Controllers;
+
 
 
 [ApiController]
@@ -12,7 +17,8 @@ public class EventController : ControllerBase
 {
 
     private readonly EventApiContext _context;
-    public EventController(EventApiContext context){
+    public EventController(EventApiContext context)
+    {
 
         _context = context;
 
@@ -24,36 +30,64 @@ public class EventController : ControllerBase
     public async Task<IActionResult> GetAllEvents()
     {
 
-       var result = await _context.Events
-       .Select(e => new {
-        Id = e.EventId,
-        Event = e.EventName,
-        Anmälda = e.Attendents.Select(a => new {
+        var result = await _context.Events
+        .Select(e => new
+        {
+            Id = e.EventId,
+            Event = e.EventName,
+            Anmälda = e.Attendents.Select(a => new
+            {
 
-            Namn = $"{a.FirstName} {a.LastName}",
-            Telefon = a.PhoneNumber
+                Namn = $"{a.FirstName} {a.LastName}",
+                Telefon = a.PhoneNumber
 
 
-        }).ToList(),
+            }).ToList(),
 
-       }).ToListAsync();
+        }).ToListAsync();
 
-       return Ok(result);
-
+        return Ok(result);
     }
 
     // Hämta event på id
     [HttpGet("{eventId}")]
     public async Task<ActionResult> GetById(int eventId)
     {
-        return Ok();
+        // bör man projicera till en viewmodel istället för anonymt objekt?
+        var result = await _context.Events
+        .Select(e => new
+        {
+            Id = e.EventId,
+            Event = e.EventName,
+            Anmälda = e.Attendents.Select(a => new
+            {
+                Namn = $"{a.FirstName} {a.LastName}",
+                Telefon = a.PhoneNumber
+            }).ToList(),
+        })
+        .SingleOrDefaultAsync(e => e.Id == eventId);
+        return Ok(result);
     }
 
     // Skapa event
     [HttpPost()]
-    public async Task<ActionResult> Create()
+    public async Task<ActionResult> Create(EventPostViewModel model)
     {
-        return StatusCode(201, new { message = "Event created" });
+        var e = new EventModel
+        {
+            EventName = model.EventName,
+            Description = model.Description,
+            StartDate = model.StartDate,
+            EndDate = model.EndDate
+        };
+
+        await _context.Events.AddAsync(e);
+
+        if (await _context.SaveChangesAsync() > 0)
+        {
+            return Created(nameof(GetById), new { id = e.EventId });
+        }
+        return StatusCode(500, "Internal Server Error");
     }
 
     // Uppdatera event
